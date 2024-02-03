19b0506f44aab3f9e8123dcb96f925083ac851e9
alejandroa1b <alejandro.carrilloamayuelas@gmail.com>
2024-02-03 20:29:15 +0100

    feat: Mejorado del enrutamiento permitiendo usar el placeholder

--- config/routes.php
+++ config/routes.php
@@ -5,12 +5,26 @@ use App\Controller\MantenimientoController;
 use App\Controller\NoticiasController;
 use App\Controller\ResultadosController;
 
+/**
+ * Configuración de Rutas del Proyecto
+ *
+ * Este archivo define todas las rutas disponibles en la aplicación y asocia cada una
+ * con un controlador y un método específico. El sistema de enrutamiento soporta el uso
+ * de comodines en las rutas para permitir la captura de parámetros dinámicos.
+ *
+ * Comodines disponibles:
+ *   {id}: Utilizado para capturar identificadores numéricos en las URLs.
+ *
+ * Ejemplo de uso:
+ *   '/noticias/{id}' se traduce en una llamada al método `show` del `NoticiasController`,
+ *   pasando el `id` como parámetro.
+ */
 return [
     '/' => [
         'controller' => HomeController::class,
         'method' => 'index'
     ],
-    '/noticias/([0-9]+)' => [
+    '/noticias/{id}' => [
         'controller' => NoticiasController::class,
         'method' => 'show'
     ],
@@ -18,7 +32,7 @@ return [
         'controller' => ResultadosController::class,
         'method' => 'list'
     ],
-    '/resultados/([0-9]+)' => [
+    '/resultados/{id}' => [
         'controller' => ResultadosController::class,
         'method' => 'show'
     ],
--- public/index.php
+++ public/index.php
@@ -9,7 +9,12 @@ $routes = require __DIR__ . '/../config/routes.php';
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $routeExists = false;
+
 foreach ($routes as $route => $dataRoute) {
+    if (strpos($route, '{id}') !== false) {
+        $route = str_replace("{id}", "([0-9]+)", $route);
+    }
+
     if (preg_match("~^$route$~", $requestUri, $matches)) {
         $routeExists = true;
         array_shift($matches);
@@ -20,7 +25,8 @@ foreach ($routes as $route => $dataRoute) {
         break;
     }
 }
-if (!$routeExists){
+
+if (!$routeExists) {
     // Ruta no encontrada, manejar el error o redireccionar
     http_response_code(404);
     require __DIR__ . '/../src/View/404.php';
