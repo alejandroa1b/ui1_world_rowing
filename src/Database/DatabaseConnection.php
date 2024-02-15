0ba1aaac54b505a40908114998554d466ade4728
Alejandro Carrillo Amayuelas <alejandro.carrillo@medac.es>
2024-02-15 14:13:39 +0100

    feat: Implementación de conexión a base de datos mediante PDO

---
+++ src/Database/DatabaseConnection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Database;
+
+use PDO;
+
+/**
+ * Clase para gestionar la conexión a la base de datos
+ */
+class DatabaseConnection
+{
+    private static $connection;
+
+    public static function getConnection()
+    {
+        if (self::$connection === null) {
+            self::$connection = new PDO('mysql:host=db;dbname=ui1_world_rowing', 'root', 'password_for_database');
+            self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        return self::$connection;
+    }
+}
